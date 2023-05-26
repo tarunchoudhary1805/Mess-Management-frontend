a6cd478ed5890800edc29c3478d201240a8bc77d
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-05-26 18:06:17 +0530

    fix: bug fix of redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const composedEnhancer = compose(composeWithDevTools(), applyMiddleware(thunk));
+const composedEnhancer =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 const initialState = {};
 
 const store = createStore(rootReducer, initialState, composedEnhancer);
